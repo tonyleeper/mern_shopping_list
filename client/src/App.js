661d12c3af1f46f73ddb3bd26c67975b09d90901
Tony Leeper <tonyleeper@blueyonder.co.uk>
2019-10-14 16:02:47 +0100

    mern stack tutorial completed part 5

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { AppNavbar } from './components/AppNavbar'
-import { ShoppingList } from './components/ShoppingList'
+import { ShoppingListContainer } from './components/ShoppingListContainer'
+
+import { Provider } from 'react-redux'
+import { store } from './store'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
 export const App = () => (
-    <div>
-        <AppNavbar />
-        <ShoppingList />
-    </div>
+    <Provider store={store}>
+        <div>
+            <AppNavbar />
+            <ShoppingListContainer />
+        </div>
+    </Provider>
 )
--- client/src/components/ShoppingListContainer.jsx
+++ client/src/components/ShoppingListContainer.jsx
@@ -2,21 +2,19 @@ import React, { Component } from 'react'
 import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap'
 import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import uuid from 'uuid'
+import { connect } from 'react-redux'
+import { getItems } from '../actions/itemActions'
+import PropTypes from 'prop-types'
 
 import styles from './ShoppingList.module.css'
 
-export class ShoppingList extends Component {
-    state = {
-        items: [
-            { id: uuid(), name: 'Eggs' },
-            { id: uuid(), name: 'Milk' },
-            { id: uuid(), name: 'Steak' },
-            { id: uuid(), name: 'Water' },
-        ],
+class ShoppingList extends Component {
+    componentDidMount() {
+        this.props.getItems()
     }
 
     render() {
-        const { items } = this.state
+        const { items } = this.props.item
         return (
             <Container>
                 <Button
@@ -66,3 +64,17 @@ export class ShoppingList extends Component {
         )
     }
 }
+
+ShoppingList.propTypes = {
+    getItems: PropTypes.func.isRequired,
+    item: PropTypes.object.isRequired,
+}
+
+const mapStateToProps = state => ({
+    item: state.item,
+})
+
+export const ShoppingListContainer = connect(
+    mapStateToProps,
+    { getItems }
+)(ShoppingList)
